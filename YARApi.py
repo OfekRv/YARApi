c328df52d01da1fb516c10974fb29a7fd24b8320
ofekr <ofekrv@gmail.com>
2022-04-09 14:19:01 +0300

    managed rules

--- YARApi.py
+++ YARApi.py
@@ -1,20 +1,28 @@
+import os
 import yara
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+#TODO: fix path-traversal
+#TODO: make thread safe (?)
 
-@app.route('/rule', methods=['GET'])
-def get_rule():
-    return "rule"
+@app.route('/rule/<name>', methods=['GET'])
+def get_rule(name):
+    with open(name + '.yar', 'r') as rule_file:
+        return rule_file.read()
 
-@app.route('/rule', methods=['POST'])
-def save_rule():
-    return "new rule"
+@app.route('/rule/<name>', methods=['POST'])
+def save_rule(name):
+    rule = request.json['content']
 
-@app.route('/rule', methods=['DELETE'])
-def delete_rule():
-    return "no rule"
+    with open(name + '.yar', 'w') as rule_file:
+        rule_file.write(rule)
+    return name
 
+@app.route('/rule/<name>', methods=['DELETE'])
+def delete_rule(name):
+    os.remove(name + '.yar')
+    return name
 
 if __name__ == '__main__':
     # TODO - no hardcoded
