33cafd7eb33512059945a560a1ebf0dedc5d5315
Ofek Reuveni <ofekrv@gmail.com>
2023-02-04 23:03:25 +0200

    most scan logc implemented

--- YARApi.py
+++ YARApi.py
@@ -1,27 +1,41 @@
-from concurrent.futures import thread
 import os
 import uuid
-from wsgiref.util import request_uri
 import yara
-import time
 import threading
-import logging
-from flask import Flask, jsonify
+from pathlib import Path
+import zipfile
+from flask import Flask, request
 
 app = Flask(__name__)
 
 PORT = os.environ.get('PORT', 5000)
-YARA_EXTENSION = ".yar"
+BASE_FOLDER = "YARA-rules"
+YARA_INDEX_FILE = "index.yar"
 
 scan_requests = {}
 scan_results = {}
 
 @app.route('/scan', methods=['POST'])
 def scan_request():
-    #TODO: verify request
+    sample = request.files['sample']
+    rules_archive = request.files['rules']
+
+    if sample.filename == '':
+        return "Sample is missing", 400
+
+    if rules_archive.filename == '':
+        return "Rules are missing", 400
+    if not zipfile.is_zipfile(rules_archive):
+        return "You must archive the rules set", 400
+
     request_uid = uuid.uuid1().hex
+    rules_path = os.path.join(BASE_FOLDER, request_uid)    
+    os.makedirs(rules_path)
+
+    with zipfile.ZipFile(rules_archive, "r") as zip_ref:
+        zip_ref.extractall(rules_path)
     scan_requests.update({request_uid: {'status': 'Pending'}})
-    threading.Thread(target=todo, args=([request_uid])).start()
+    threading.Thread(target=scan, args=(request_uid, sample, rules_path)).start()
     return {'location': '/requests/' + request_uid + '/status'}, 202
 
 @app.route('/requests/<request_id>/status', methods=['GET'])
@@ -39,43 +53,27 @@ def scan_result(result_id):
     resource = scan_results[result_id];
     return resource, 200
 
-def todo(request_id):
-    time.sleep(5)
+def scan(request_id, sample, rules_path):
+    index_file = search_file(rules_path, YARA_INDEX_FILE)
+    rules = yara.compile(index_file, includes=True)
+    matches = rules.match(sample)
+    os.rmdir(rules_path)
+    submit_result(request_id, matches)
+
+def submit_result(request_id, result):
     scan_request = scan_requests[request_id]
     result_uid = uuid.uuid1().hex
-    scan_results.update({result_uid: 'TODO'})
+    scan_results.update({result_uid: result})
     scan_request['status'] = 'Completed'
     scan_request['result'] = '/results/' + result_uid
     scan_requests.update({request_id: scan_request})
 
-def init_rules():
-    logging.info('initing rules')
-    
-    # TODO: rules source from github url (?)
-    rules = []
-
-    for rule in rules:
-        save_rule_file(rule['name'], rule['raw'])
-    generate_index_rule()
-    
-    logging.info('rules initialized')
-
-def generate_index_rule():
-    logging.info('generating index rule')
-    if os.path.exists('index'):
-        os.remove('index')   
-    
-    with open('index', 'w') as rules:
-        for rule_file in os.scandir(os.curdir):
-            if rule_file.path.endswith(YARA_EXTENSION):
-                rules.write('include \"' + rule_file.name + '\"\n')
-
-    yara.compile(filepath='index', includes=True).save('compiled_index')
-    logging.info('index rule compiled')
-
-def save_rule_file(name, content):
-    with open(name + YARA_EXTENSION, 'w') as rule_file:
-        rule_file.write(content)
+def search_file(root, file_name):
+    for dirpath, dirnames, filenames in os.walk(root):
+        for name in filenames:
+            if name == file_name:
+                return os.path.join(dirpath, name)
+    return None
 
 if __name__ == '__main__':
     app.run(threaded=True, port=PORT)
