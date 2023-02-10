2ac23b45a552afbd0273503092f4e98fcd1509df
Ofek Reuveni <ofekrv@gmail.com>
2023-02-10 17:30:14 +0200

    common yara request logic extracted - prepare for same logic for both apis

--- YARApi.py
+++ YARApi.py
@@ -22,10 +22,10 @@ RULES_FOLDER = os.environ.get('RULES_FOLDER', 'YARA-rules')
 YARA_INDEX_FILE = os.environ.get('YARA_INDEX_FILE', 'index.yar')
 SAMPLE_FILE = os.environ.get('SAMPLE_FILE', 'sample.dnr')
 YARA_MAX_STRING_PER_RULE = os.environ.get('YARA_MAX_STRING_PER_RULE', 5000000)
-CHATBOT_TOKEN = os.getenv('CHATBOT_TOKEN', '*')
+CHATBOT_TOKEN = os.getenv('CHATBOT_TOKEN', 'MTA3MzQ5MTczODExMjU2NTI3MA.GKQy-U.xQQB_XOiwqTIe3xiyPkLIemmfNdVFOrtvO1QD8')
 CHATBOT_COMMAND_PREFIX = os.getenv('CHATBOT_COMMAND_PREFIX ', '/')
-GUILD = os.getenv('CHATBOT_DISCORD_GUILD', '*')
-SCAN_CHANNEL = os.getenv('SCAN_CHANNEL ', 0)
+GUILD = os.getenv('CHATBOT_DISCORD_GUILD', '1073492750428807199')
+SCAN_CHANNEL = os.getenv('SCAN_CHANNEL ', 1073526354924011580)
 
 yara.set_config(max_strings_per_rule=YARA_MAX_STRING_PER_RULE)
 
@@ -36,33 +36,20 @@ scan_results = {}
 async def on_ready():
     await chatbot_command_tree.sync(guild=discord.Object(id=GUILD))
     
-@chatbot_command_tree.command(name = "scan", description = "Scan a file with your own rules set", guild=discord.Object(id=GUILD)) 
+@chatbot_command_tree.command(name = "scan",
+                              description = "Scan a file with your own rules set",
+                              guild=discord.Object(id=GUILD)) 
 async def scan_request(interaction, sample: discord.Attachment, rules_archive: discord.Attachment):
     if interaction.channel_id != SCAN_CHANNEL:
         await interaction.response.send_message("wrong channel, please switch to scanner channel :)")
         return    
-    
-    request_uid = uuid.uuid1().hex
-    request_files_path = os.path.join(BASE_FOLDER, request_uid)    
-    rules_path = os.path.join(request_files_path, RULES_FOLDER)    
-    os.makedirs(rules_path)
-    
-    rules_archive_path  = os.path.join(request_files_path, rules_archive.filename)
-    await rules_archive.save(rules_archive_path)
-
-    if not zipfile.is_zipfile(rules_archive_path):
-        await interaction.response.send_message("rules set must be an archive!")
-        return
-
-    with zipfile.ZipFile(rules_archive_path, "r") as zip_ref:
-        zip_ref.extractall(rules_path)
-    
-    sample_path = os.path.join(request_files_path, SAMPLE_FILE)
-    await sample.save(sample_path)
-
-    scan_requests.update({request_uid: {'status': 'Pending'}})
-    formatted_result =  '`' + pprint.pformat(scan(request_uid, sample_path, rules_path), 2) + '`'
-    await interaction.response.send_message(formatted_result)
+    try:
+        result = await generate_scan_request_result(sample, rules_archive)
+    #TODO: create custom exception
+    except Exception as e:
+        await interaction.response.send_message(str(e))
+    else:
+        await interaction.response.send_message(format_chat_output(result))
 
 @app.route('/scan', methods=['POST'])
 def scan_request():
@@ -75,7 +62,7 @@ def scan_request():
     if rules_archive.filename == '':
         return "Rules are missing", 400
     if not zipfile.is_zipfile(rules_archive):
-        return "You must archive the rules set", 400
+        return "rules set must be an archive!", 400
 
     request_uid = uuid.uuid1().hex
     rules_path = os.path.join(BASE_FOLDER, request_uid, RULES_FOLDER)    
@@ -106,6 +93,45 @@ def scan_result(result_id):
     resource = scan_results[result_id];
     return resource, 200
 
+async def generate_scan_request_result(sample, rules_archive):
+    request_id, sample_path, rules_path = await submit_request(sample, rules_archive)
+    result = scan(request_id, sample_path, rules_path)
+    return result
+
+async def generate_request_files(request_id, sample, rules_archive):
+    if sample.filename == '':
+        raise FileNotFoundError("Sample is missing!")
+    if rules_archive.filename == '':
+        raise FileNotFoundError("Rules are missing!")
+    
+    request_folder = os.path.join(BASE_FOLDER, request_id)
+    rules_path = os.path.join(request_folder, RULES_FOLDER)    
+    os.makedirs(rules_path)
+
+    rules_archive_path = os.path.join(request_folder, rules_archive.filename)
+    await rules_archive.save(rules_archive_path)
+    sample_path = os.path.join(request_folder,SAMPLE_FILE)
+    await sample.save(sample_path)
+    
+    if not zipfile.is_zipfile(rules_archive_path):
+        raise TypeError("rules set must be an archive!")
+
+    with zipfile.ZipFile(rules_archive_path, "r") as zip_ref:
+        zip_ref.extractall(rules_path)
+
+    return sample_path, rules_path
+
+async def handle_scan_request(sample, rules_archive):
+    request_id, sample_path, rules_path = submit_request(sample, rules_archive)
+    threading.Thread(target=scan, args=(request_id, sample_path, rules_path)).start()
+    return {'location': '/requests/' + request_id + '/status'}, 202
+
+async def submit_request(sample, rules_archive):
+    request_id = uuid.uuid1().hex
+    sample_path, rules_path = await generate_request_files(request_id, sample, rules_archive)
+    scan_requests.update({request_id: {'status': 'Pending'}})
+    return request_id, sample_path, rules_path
+
 def scan(request_id, sample_path, rules_path):
     index_file = search_file(rules_path, YARA_INDEX_FILE)
     rules = yara.compile(index_file, includes=True)
@@ -132,6 +158,9 @@ def search_file(root, file_name):
             if name == file_name:
                 return os.path.join(dirpath, name)
     return None
+
+def format_chat_output(output):
+    return '`' + pprint.pformat(output, 2) + '`'
 
 if __name__ == '__main__':
     chatbot_client.run(CHATBOT_TOKEN)
