ea5abc7c1e2d438f3b5c94c10b15606a8dd85b5d
Ofek Reuveni <ofekrv@gmail.com>
2023-02-11 10:46:11 +0200

    rest yara updated

--- YARApi.py
+++ YARApi.py
@@ -1,3 +1,7 @@
+from YARApiError import YARApiError, YARApiFileNotFoundError, YARApiRulesFileTypeError
+from discord import app_commands
+from discord.ext import commands
+from flask import Flask, request
 import os
 import uuid
 import yara
@@ -6,9 +10,6 @@ import threading
 import zipfile
 import shutil
 import discord
-from discord import app_commands
-from discord.ext import commands
-from flask import Flask, request
 
 app = Flask(__name__)
 intents = discord.Intents.default()
@@ -22,9 +23,9 @@ RULES_FOLDER = os.environ.get('RULES_FOLDER', 'YARA-rules')
 YARA_INDEX_FILE = os.environ.get('YARA_INDEX_FILE', 'index.yar')
 SAMPLE_FILE = os.environ.get('SAMPLE_FILE', 'sample.dnr')
 YARA_MAX_STRING_PER_RULE = os.environ.get('YARA_MAX_STRING_PER_RULE', 5000000)
-CHATBOT_TOKEN = os.getenv('CHATBOT_TOKEN', '*')
+CHATBOT_TOKEN = os.getenv('CHATBOT_TOKEN', '0')
 CHATBOT_COMMAND_PREFIX = os.getenv('CHATBOT_COMMAND_PREFIX ', '/')
-GUILD = os.getenv('CHATBOT_DISCORD_GUILD', '*')
+GUILD = os.getenv('CHATBOT_DISCORD_GUILD', '0')
 SCAN_CHANNEL = os.getenv('SCAN_CHANNEL ', 0)
 
 yara.set_config(max_strings_per_rule=YARA_MAX_STRING_PER_RULE)
@@ -44,39 +45,20 @@ async def scan_request(interaction, sample: discord.Attachment, rules_archive: d
         await interaction.response.send_message("wrong channel, please switch to scanner channel :)")
         return    
     try:
-        result = await generate_scan_request_result(sample, rules_archive)
-    #TODO: create custom exception
-    except Exception as e:
+        result = await generate_scan_request_result(sample, rules_archive, save_attachment)
+    except YARApiError as e:
         await interaction.response.send_message(str(e))
     else:
         await interaction.response.send_message(format_chat_output(result))
 
 @app.route('/scan', methods=['POST'])
-def scan_request():
+async def scan_request():
     sample = request.files['sample']
     rules_archive = request.files['rules']
-
-    if sample.filename == '':
-        return "Sample is missing", 400
-
-    if rules_archive.filename == '':
-        return "Rules are missing", 400
-    if not zipfile.is_zipfile(rules_archive):
-        return "rules set must be an archive!", 400
-
-    request_uid = uuid.uuid1().hex
-    rules_path = os.path.join(BASE_FOLDER, request_uid, RULES_FOLDER)    
-    os.makedirs(rules_path)
-
-    with zipfile.ZipFile(rules_archive, "r") as zip_ref:
-        zip_ref.extractall(rules_path)
-    
-    sample_path = os.path.join(BASE_FOLDER, request_uid, SAMPLE_FILE)
-    sample.save(sample_path)
-
-    scan_requests.update({request_uid: {'status': 'Pending'}})
-    threading.Thread(target=scan, args=(request_uid, sample_path, rules_path)).start()
-    return {'location': '/requests/' + request_uid + '/status'}, 202
+    try:
+        return await handle_scan_request(sample, rules_archive, save_file)
+    except YARApiError as e:
+        return str(e), 400
 
 @app.route('/requests/<request_id>/status', methods=['GET'])
 def request_status(request_id):
@@ -93,42 +75,44 @@ def scan_result(result_id):
     resource = scan_results[result_id];
     return resource, 200
 
-async def generate_scan_request_result(sample, rules_archive):
-    request_id, sample_path, rules_path = await submit_request(sample, rules_archive)
+async def generate_scan_request_result(sample, rules_archive, save_method):
+    request_id, sample_path, rules_path = await submit_request(sample, rules_archive, save_method)
     result = scan(request_id, sample_path, rules_path)
     return result
 
-async def generate_request_files(request_id, sample, rules_archive):
+async def generate_request_files(request_id, sample, rules_archive, save_method):
     if sample.filename == '':
-        raise FileNotFoundError("Sample is missing!")
+        raise YARApiFileNotFoundError("Sample is missing!")
     if rules_archive.filename == '':
-        raise FileNotFoundError("Rules are missing!")
+        raise YARApiFileNotFoundError("Rules are missing!")
     
     request_folder = os.path.join(BASE_FOLDER, request_id)
     rules_path = os.path.join(request_folder, RULES_FOLDER)    
     os.makedirs(rules_path)
 
     rules_archive_path = os.path.join(request_folder, rules_archive.filename)
-    await rules_archive.save(rules_archive_path)
+    #await rules_archive.save(rules_archive_path)
+    await save_method(rules_archive, rules_archive_path)
     sample_path = os.path.join(request_folder,SAMPLE_FILE)
-    await sample.save(sample_path)
+    #await sample.save(sample_path)
+    await save_method(sample, sample_path)
     
     if not zipfile.is_zipfile(rules_archive_path):
-        raise TypeError("rules set must be an archive!")
+        raise YARApiRulesFileTypeError("rules set must be an archive!")
 
     with zipfile.ZipFile(rules_archive_path, "r") as zip_ref:
         zip_ref.extractall(rules_path)
 
     return sample_path, rules_path
 
-async def handle_scan_request(sample, rules_archive):
-    request_id, sample_path, rules_path = submit_request(sample, rules_archive)
+async def handle_scan_request(sample, rules_archive, save_method):
+    request_id, sample_path, rules_path = await submit_request(sample, rules_archive, save_method)
     threading.Thread(target=scan, args=(request_id, sample_path, rules_path)).start()
     return {'location': '/requests/' + request_id + '/status'}, 202
 
-async def submit_request(sample, rules_archive):
+async def submit_request(sample, rules_archive, save_method):
     request_id = uuid.uuid1().hex
-    sample_path, rules_path = await generate_request_files(request_id, sample, rules_archive)
+    sample_path, rules_path = await generate_request_files(request_id, sample, rules_archive, save_method)
     scan_requests.update({request_id: {'status': 'Pending'}})
     return request_id, sample_path, rules_path
 
@@ -139,7 +123,13 @@ def scan(request_id, sample_path, rules_path):
     shutil.rmtree(os.path.join(BASE_FOLDER, request_id))
     rules_matched = []
     for match in matches:
-        rules_matched.append({'rule': match.rule, 'meta': match.meta, 'strings': match.strings})    
+        matched_strings = []
+        #TODO: data class for matched strings
+        for matched_string in match.strings:
+            matched_strings.append(str(matched_string))
+        rules_matched.append({'rule': match.rule,
+                              'meta': match.meta,
+                              'strings': matched_strings})
     result = {'matches': rules_matched}
     submit_result(request_id, result)
     return result
@@ -162,6 +152,11 @@ def search_file(root, file_name):
 def format_chat_output(output):
     return '`' + pprint.pformat(output, 2) + '`'
 
+async def save_attachment(file, path):
+    await file.save(path)
+async def save_file(file, path):
+    file.save(path)
+
 if __name__ == '__main__':
-    chatbot_client.run(CHATBOT_TOKEN)
+    #chatbot_client.run(CHATBOT_TOKEN)
     app.run(threaded=True, port=PORT)
