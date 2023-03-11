16e782af0b5e47db3e9b1124a006cabd4913191d
Ofek Reuveni <ofekrv@gmail.com>
2023-03-11 11:57:04 +0200

    single rule scan

--- YARApi/managers/scan_manager.py
+++ YARApi/managers/scan_manager.py
@@ -6,47 +6,34 @@ import zipfile
 from errors.YARApiFileNotFoundError import YARApiFileNotFoundError
 from errors.YARApiRulesFileTypeError import YARApiRulesFileTypeError
 from scanners import YARAScanner
+from utils.files_util import search_file
 
 BASE_FOLDER = os.environ.get('BASE_FOLDER', default='Uploads')
 RULES_FOLDER = os.environ.get('RULES_FOLDER', default='YARA-rules')
 SAMPLE_FILE = os.environ.get('SAMPLE_FILE', default='sample.dnr')
+YARA_INDEX_FILE = os.environ.get('YARA_INDEX_FILE', default='index.yar')
 
 scan_requests = {}
 scan_results = {}
 
-async def submit_request(sample, rules_archive, save_method):
+async def submit_request(sample, rules_archive, single_rule_file, save_method):
     request_id = uuid.uuid1().hex
-    sample_path, rules_path = await generate_request_files(request_id, sample, rules_archive, save_method)
+    sample_path, rule_path = await __generate_request_files(request_id,
+                                                            sample,
+                                                            rules_archive,
+                                                            single_rule_file,
+                                                            save_method)
     scan_requests.update({request_id: {'status': 'Pending'}})
-    return request_id, sample_path, rules_path
+    return request_id, sample_path, rule_path
 
-async def handle_scan_request(sample, rules_archive, save_method):
-    request_id, sample_path, rules_path = await submit_request(sample, rules_archive, save_method)
-    threading.Thread(target=YARAScanner.scan, args=(request_id, sample_path, rules_path)).start()
+async def generate_scan_request_result(sample, rules_archive, single_rule_file, save_method):
+    request_id, sample_path, rule_path = await submit_request(sample, rules_archive, single_rule_file, save_method)
+    return YARAScanner.scan(request_id, sample_path, rule_path)
+
+async def handle_scan_request(sample, rules_archive, single_rule_file, save_method):
+    request_id, sample_path, rule_path = await submit_request(sample, rules_archive, single_rule_file, save_method)
+    threading.Thread(target=YARAScanner.scan, args=(request_id, sample_path, rule_path)).start()
     return {'location': '/requests/' + request_id + '/status'}, 202
-
-async def generate_request_files(request_id, sample, rules_archive, save_method):
-    if sample.filename == '':
-        raise YARApiFileNotFoundError("Sample is missing!")
-    if rules_archive.filename == '':
-        raise YARApiFileNotFoundError("Rules are missing!")
-    
-    request_folder = os.path.join(BASE_FOLDER, request_id)
-    rules_path = os.path.join(request_folder, RULES_FOLDER)    
-    os.makedirs(rules_path)
-
-    rules_archive_path = os.path.join(request_folder, rules_archive.filename)
-    await save_method(rules_archive, rules_archive_path)
-    sample_path = os.path.join(request_folder, SAMPLE_FILE)
-    await save_method(sample, sample_path)
-    
-    if not zipfile.is_zipfile(rules_archive_path):
-        raise YARApiRulesFileTypeError("rules set must be an archive!")
-
-    with zipfile.ZipFile(rules_archive_path, "r") as zip_ref:
-        zip_ref.extractall(rules_path)
-
-    return sample_path, rules_path
 
 def submit_result(request_id, result):
     scan_request = scan_requests[request_id]
@@ -55,3 +42,37 @@ def submit_result(request_id, result):
     scan_request['status'] = 'Completed'
     scan_request['result'] = '/results/' + result_uid
     scan_requests.update({request_id: scan_request})
+
+async def __generate_request_files(request_id, sample, rules_archive, single_rule_file, save_method):
+    if sample.filename == '':
+        raise YARApiFileNotFoundError("Could not find sample")
+    if (rules_archive is None or rules_archive.filename == '') and (single_rule_file is None or single_rule_file.filename == ''):
+        raise YARApiFileNotFoundError("Could not find either rules archive nor single rule")
+    
+    request_folder = os.path.join(BASE_FOLDER, request_id)
+    rules_path = os.path.join(request_folder, RULES_FOLDER)    
+    os.makedirs(rules_path)
+
+    sample_path = os.path.join(request_folder, SAMPLE_FILE)
+    await save_method(sample, sample_path)
+
+    if rules_archive.filename != '':
+        rule_path = await __generate_rules_files(rules_archive, save_method, request_folder, rules_path)
+    else:
+        rule_path = await save_method(single_rule_file, os.path.join(rules_path, YARA_INDEX_FILE))
+
+    return sample_path, rule_path
+
+async def __generate_rules_files(rules_archive, save_method, request_folder, rules_path):
+    rules_archive_path = os.path.join(request_folder, rules_archive.filename)
+    await save_method(rules_archive, rules_archive_path)
+    
+    if not zipfile.is_zipfile(rules_archive_path):
+        raise YARApiRulesFileTypeError("rules set must be an archive!")
+    with zipfile.ZipFile(rules_archive_path, "r") as zip_ref:
+        zip_ref.extractall(rules_path)
+
+    index_rule = search_file(rules_path, YARA_INDEX_FILE)
+    if index_rule == '':
+        raise YARApiFileNotFoundError('Could not find index rule, must be named "' + YARA_INDEX_FILE + '"')
+    return index_rule
--- YARApi/scanners/YARAScanner.py
+++ YARApi/scanners/YARAScanner.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import pprint
 import shutil
 
 import yara
@@ -8,25 +7,23 @@ from errors.YARApiRulesFileSyntaxError import YARApiRulesFileSyntaxError
 from yara import SyntaxError
 
 BASE_FOLDER = os.environ.get('BASE_FOLDER', default='Uploads')
-
-YARA_INDEX_FILE = os.environ.get('YARA_INDEX_FILE', default='index.yar')
 YARA_MAX_STRING_PER_RULE = os.environ.get('YARA_MAX_STRING_PER_RULE', default=5000000)
 
 yara.set_config(max_strings_per_rule=YARA_MAX_STRING_PER_RULE)
 
-def scan(request_id, sample_path, rules_path):
+def scan(request_id, sample_path, rule):
     logging.info('start scanning request ' + request_id)
-    index_file = search_file(rules_path, YARA_INDEX_FILE)
-    logging.info('found index file of request ' + request_id)
     try:
-        rules = yara.compile(index_file, includes=True)
+        rules = yara.compile(rule, includes=True)
     except SyntaxError as e:
         raise YARApiRulesFileSyntaxError(e) from None
     logging.info('rules request ' + request_id + 'compiled successfully')
     matches = rules.match(sample_path)
     logging.info('scan of request ' + request_id + 'finished')
-    shutil.rmtree(os.path.join(BASE_FOLDER, request_id))
-    logging.info('files request ' + request_id + 'deleted successfully')
+    __delete_request_files(request_id)
+    return __build_scan_response(matches) 
+
+def __build_scan_response(matches):
     rules_matched = []
     for match in matches:
         matched_strings = []
@@ -36,19 +33,8 @@ def scan(request_id, sample_path, rules_path):
         rules_matched.append({'rule': match.rule,
                               'meta': match.meta,
                               'strings': matched_strings})
-    return {'matches': rules_matched}    
+    return {'matches': rules_matched}
 
-def search_file(root, file_name):
-    for dirpath, dirnames, filenames in os.walk(root):
-        for name in filenames:
-            if name == file_name:
-                return os.path.join(dirpath, name)
-    return None
-
-def format_chat_output(output):
-    return '`' + pprint.pformat(output, 2) + '`'
-
-async def save_attachment(file, path):
-    await file.save(path)
-async def save_file(file, path):
-    file.save(path)
+def __delete_request_files(request_id):
+    shutil.rmtree(os.path.join(BASE_FOLDER, request_id))
+    logging.info('files request ' + request_id + 'deleted successfully')  
